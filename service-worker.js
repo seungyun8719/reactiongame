c7ed8530dcaf0830702a0569622382cbcc3a7ad5
seungyun8719 <shinseungyun8719@gmail.com>
2026-02-14 16:34:21 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+
+const CACHE_NAME = "reaction-game-v4";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon.png"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
